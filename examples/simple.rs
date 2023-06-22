62fc7a0bc643d502b30749134792085d195cab85
tyush <tyushk@gmail.com>
2023-06-22 04:05:44 -0500

    Show callback functionality in example.

--- examples/simple.rs
+++ examples/simple.rs
@@ -3,14 +3,36 @@
 // SPDX-License-Identifier: Apache-2.0
 // SPDX-License-Identifier: MIT
 
-use tauri_winrt_notification::{Duration, Sound, Toast};
+use std::sync::{Barrier, OnceLock};
+
+use tauri_winrt_notification::{Duration, Sound, Toast, ToastNotification};
+use windows::core::IInspectable;
+
+static GATE: OnceLock<Barrier> = OnceLock::new();
 
 fn main() {
+    GATE.set(Barrier::new(2)).unwrap();
+
     Toast::new(Toast::POWERSHELL_APP_ID)
         .title("Look at this flip!")
         .text1("(╯°□°）╯︵ ┻━┻")
         .sound(Some(Sound::SMS))
         .duration(Duration::Short)
+        .on_activated(handle_notification_click)
         .show()
         .expect("unable to send notification");
+    println!("Waiting for the notification to be clicked...");
+
+    GATE.get().unwrap().wait();
+}
+
+fn handle_notification_click(
+    toast: &Option<ToastNotification>,
+    object: &Option<IInspectable>,
+) -> windows::core::Result<()> {
+    println!("You've clicked me!");
+    dbg!(toast);
+    dbg!(object);
+    GATE.get().unwrap().wait();
+    Ok(())
 }
