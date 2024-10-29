5ef6983dc190803459c49791f4d2b72ed2c036bf
iKineticate <115683118+iKineticate@users.noreply.github.com>
2024-10-29 18:59:16 +0800

    fix: address Clippy warnings

--- src/lib.rs
+++ src/lib.rs
@@ -652,30 +652,28 @@ impl Toast {
     /// use std::{thread::sleep, time::Duration as StdDuration};
     /// use tauri_winrt_notification::{Toast, Progress};
     ///
-    /// fn main() {
-    ///     let mut progress = Progress {
-    ///         tag: "my_tag".to_string(),
-    ///         title: "video.mp4".to_string(),
-    ///         status: "Transferring files...".to_string(),
-    ///         value: 0.0,
-    ///         value_string: "0/1000 MB".to_string(),
+    /// let mut progress = Progress {
+    ///     tag: "my_tag".to_string(),
+    ///     title: "video.mp4".to_string(),
+    ///     status: "Transferring files...".to_string(),
+    ///     value: 0.0,
+    ///     value_string: "0/1000 MB".to_string(),
+    /// };
+    ///
+    /// let toast = Toast::new(Toast::POWERSHELL_APP_ID).progress(&progress);
+    /// toast.show().expect("notification failed");
+    ///
+    /// for i in 1..=10 {
+    ///     sleep(StdDuration::from_secs(1));
+    ///         
+    ///     progress.value = i as f32 / 10.0;
+    ///     progress.value_string = format!("{}/1000 MB", i * 100);
+    ///
+    ///     if i == 10 {
+    ///         progress.status = String::from("Completed!");
     ///     };
     ///
-    ///     let toast = Toast::new(Toast::POWERSHELL_APP_ID).progress(&progress);
-    ///     toast.show().expect("notification failed");
-    ///
-    ///     for i in 1..=10 {
-    ///         sleep(StdDuration::from_secs(1));
-    ///         
-    ///         progress.value = i as f32 / 10.0;
-    ///         progress.value_string = format!("{}/1000 MB", i * 100);
-    ///
-    ///         if i == 10 {
-    ///             progress.status = String::from("Completed!");
-    ///         };
-    ///
-    ///         toast.set_progress(&progress).expect("failed to set notification progress");
-    ///     }
+    ///     toast.set_progress(&progress).expect("failed to set notification progress");
     /// }
     /// ```
     pub fn set_progress(&self, progress: &Progress) -> Result<NotificationUpdateResult> {
