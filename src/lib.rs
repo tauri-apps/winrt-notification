09a4135a871c6ead29832dca5698948fb30bf94b
Fabian-Lars <30730186+FabianLars@users.noreply.github.com>
2026-07-02 12:01:23 +0000

    refactor: inline quick-xml::escape::escape (#53)
    * refactor: inline quick-xml::escape::escape
    
    * don't use it in examples either

--- src/lib.rs
+++ src/lib.rs
@@ -31,6 +31,9 @@
 //! * <https://softwareengineering.stackexchange.com/questions/222339/using-the-system-tray-notification-area-app-in-windows-7>
 //!
 //! For actions look at <https://docs.microsoft.com/en-us/dotnet/api/microsoft.toolkit.uwp.notifications.toastactionscustom?view=win-comm-toolkit-dotnet-7.0>
+
+mod xml_escape;
+
 use windows::{
     core::{IInspectable, Interface},
     Data::Xml::Dom::XmlDocument,
@@ -356,10 +359,7 @@ impl Toast {
     /// Will be white.
     /// Supports Unicode ✓
     pub fn title(mut self, content: &str) -> Toast {
-        self.title = format!(
-            r#"<text id="1">{}</text>"#,
-            &quick_xml::escape::escape(content)
-        );
+        self.title = format!(r#"<text id="1">{}</text>"#, xml_escape::escape(content));
         self
     }
 
@@ -368,10 +368,7 @@ impl Toast {
     /// Will be grey.
     /// Supports Unicode ✓
     pub fn text1(mut self, content: &str) -> Toast {
-        self.line1 = format!(
-            r#"<text id="2">{}</text>"#,
-            &quick_xml::escape::escape(content)
-        );
+        self.line1 = format!(r#"<text id="2">{}</text>"#, xml_escape::escape(content));
         self
     }
 
@@ -380,10 +377,7 @@ impl Toast {
     /// Will be grey.
     /// Supports Unicode ✓
     pub fn text2(mut self, content: &str) -> Toast {
-        self.line2 = format!(
-            r#"<text id="3">{}</text>"#,
-            &quick_xml::escape::escape(content)
-        );
+        self.line2 = format!(r#"<text id="3">{}</text>"#, xml_escape::escape(content));
         self
     }
 
@@ -427,8 +421,8 @@ impl Toast {
                 r#"{}<image placement="appLogoOverride" {} src="file:///{}" alt="{}" />"#,
                 self.images,
                 crop_type_attr,
-                quick_xml::escape::escape(source.display().to_string()),
-                quick_xml::escape::escape(alt_text)
+                xml_escape::escape(source.display().to_string()),
+                xml_escape::escape(alt_text)
             );
             self
         } else {
@@ -445,8 +439,8 @@ impl Toast {
             self.images = format!(
                 r#"{}<image placement="Hero" src="file:///{}" alt="{}" />"#,
                 self.images,
-                quick_xml::escape::escape(source.display().to_string()),
-                quick_xml::escape::escape(alt_text)
+                xml_escape::escape(source.display().to_string()),
+                xml_escape::escape(alt_text)
             );
             self
         } else {
@@ -467,8 +461,8 @@ impl Toast {
         self.images = format!(
             r#"{}<image id="1" src="file:///{}" alt="{}" />"#,
             self.images,
-            quick_xml::escape::escape(source.display().to_string()),
-            quick_xml::escape::escape(alt_text)
+            xml_escape::escape(source.display().to_string()),
+            xml_escape::escape(alt_text)
         );
         self
     }
