2928e609cf62b866c2043bc2edea5310428acf0f
Fabian-Lars <30730186+FabianLars@users.noreply.github.com>
2026-07-06 13:32:26 +0200

    fmt

--- examples/image.rs
+++ examples/image.rs
@@ -3,7 +3,7 @@
 // SPDX-License-Identifier: Apache-2.0
 // SPDX-License-Identifier: MIT
 
-use std::path::{Path, absolute};
+use std::path::{absolute, Path};
 use tauri_winrt_notification::{IconCrop, Toast};
 
 fn main() {
@@ -15,11 +15,7 @@ fn main() {
     let icon_path = absolute(Path::new("./examples/icon.png")).unwrap();
     Toast::new(Toast::POWERSHELL_APP_ID)
         .hero(&icon_path, "alt text")
-        .icon(
-            &icon_path,
-            IconCrop::Circular,
-            "alt text",
-        )
+        .icon(&icon_path, IconCrop::Circular, "alt text")
         .title("Lots of pictures here")
         .text1("One above the text as the hero")
         .text2("One to the left as an icon, and several below")
